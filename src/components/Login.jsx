ac3c631e49a5618ccedbe5778ef7eb7aa72c243d
nagma <bano.nagma.1@gmail.com>
2025-10-23 13:58:16 +0530

    handling form submission

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,12 @@
 export default function Login() {
 
-  function handleSubmit() {
+  function handleSubmit(event) {
+    event.preventDefault();
     console.log("Submitted!");
   }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
@@ -22,7 +23,7 @@ export default function Login() {
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button type="button" className="button" onClick={handleSubmit}>Login</button>
+        <button className="button" onClick={handleSubmit}>Login</button>
       </p>
     </form>
   );
