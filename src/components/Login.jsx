3947b3a2f492056bccc7dda0900429810cd08b8a
nagma <bano.nagma.1@gmail.com>
2025-10-23 14:52:22 +0530

    form handling using state

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,35 +1,9 @@
-import { useState } from "react";
-
 export default function Login() {
-
-  // const [enteredEmail, setEnteredEmail] = useState('');
-  // const [enteredPassword, setEnteredPassword] = useState('');
-
-  const [enteredValue, setEnteredValue] = useState({
-    email: '',
-    password: ''
-  });
 
   function handleSubmit(event) {
     event.preventDefault();
     console.log("Submitted!");
-    console.log(enteredValue);
   }
-
-  function handleInputChange(identifier, value) {
-    setEnteredValue((preValue) => ({
-      ...preValue,
-      [identifier]: value
-    }));
-  }
-
-  // function handleEmailChange(event) {
-  //   setEnteredEmail(event.target.value);
-  // }
-
-  // function handlePasswordChange(event) {
-  //   setEnteredPassword(event.target.value);
-  // }
 
   return (
     <form onSubmit={handleSubmit}>
@@ -38,18 +12,18 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" onChange={(event)=>handleInputChange('email', event.target.value)} value={enteredValue.email} />
+          <input id="email" type="email" name="email"/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" onChange={(event)=>handleInputChange('password', event.target.value)} value={enteredValue.password} />
+          <input id="password" type="password" name="password"/>
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button className="button" onClick={handleSubmit}>Login</button>
+        <button className="button">Login</button>
       </p>
     </form>
   );
