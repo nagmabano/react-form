a1a0d5d8438168e44d41829734a29285b4698276
nagma <bano.nagma.1@gmail.com>
2025-10-24 13:03:50 +0530

    validating input on form submission

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,7 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 export default function Login() {
+
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
 
   const email = useRef();
   const password = useRef();
@@ -11,17 +13,25 @@ export default function Login() {
     const enteredpassword = password.current.value;
 
     console.log(enteredEmail, enteredpassword);
-    
+
+    const emailIsValid = enteredEmail.includes('@');
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form noValidate onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" ref={email}/>
+          <div className="control-error">{emailIsInvalid && <p>Please enter a valid email.</p>}</div>
         </div>
 
         <div className="control no-margin">
@@ -32,7 +42,7 @@ export default function Login() {
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+        <button className="button" >Login</button>
       </p>
     </form>
   );
